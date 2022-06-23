b927cf393747567446848f835bc0b8215c69e59d
Sathish Ramasami <103182679+sathish0143@users.noreply.github.com>
2022-06-23 18:34:17 +0530

    Add files via upload

---
+++ stringsort.js
@@ -0,0 +1,8 @@
+//Insertionsort
+console.clear();
+
+var exampleString= ["vijay", "surya", "danush", "ajith", "karthik","sathish"];
+console.log("Original String");
+console.log(exampleString);
+exampleString.sort();
+console.log(exampleString);
